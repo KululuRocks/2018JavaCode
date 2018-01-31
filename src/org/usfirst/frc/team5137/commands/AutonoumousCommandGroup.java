b80f1f2a59ebc650c496df4030b20aa0136a51f6
VinceBartels <VinceBartels@users.noreply.github.com>
2018-01-30 20:49:57 -0800

    added comments to the robot java

--- src/org/usfirst/frc/team5137/commands/AutonoumousCommandGroup.java
+++ src/org/usfirst/frc/team5137/commands/AutonoumousCommandGroup.java
@@ -4,13 +4,11 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class AutonoumousCommandGroup extends CommandGroup {
 	
-	public void AutonomousCommandGroup() {
+	public AutonoumousCommandGroup() {
 	
-		System.out.println("HIIIIIIIII");
-		//addSequential(new DriveStraight());
-		//addSequential(new DisplayValues());
-		addSequential(new DriveStraight());
-		
+		System.out.println("HIIIIIIIII"); // This is a check to see if the autonomous periodic is actually running
+		addSequential(new DriveStraight());// This is the command it will run first
+		addParallel(new DisplayValues());	// The command it will run in parallel to the one above
 		
 	}
 		
--- src/org/usfirst/frc/team5137/robot/Robot.java
+++ src/org/usfirst/frc/team5137/robot/Robot.java
@@ -14,8 +14,6 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
-//import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -25,22 +23,43 @@ import edu.wpi.first.wpilibj.command.Scheduler;
  * project.
  */
 public class Robot extends TimedRobot {
-	public static DriveBase driveBase;
-	public static OI oi;
-	public static Timer timer = new Timer();
+	public static DriveBase driveBase; 
+	public static OI oi;	
+	/*	The Robot class acts as a kind of hub for all other classes, including subsystems and commands. 
+	 * For example, by stating that the DriveBase is static and public, 
+	 * it allows the whole robot program to find it.  With out doing this, no class could find another.
+	 */
 	
-	Command autonomousCommand;
+	public static Timer timer = new Timer(); 
+	/* This creates a timer that the whole Robot can see and can be used to run commands off of
+	 i.e. the AutoDrive command does.
+	*/
+	
+	Command autonomousCommand; 
+	// This tells the Robot that there is an autonomousCommand 
+	 
 
 	public void robotInit() {
+		
+		/*	robotInit() is the first thing the robot does on boot up
+		 it is used to declare what subsytems and the oi are and to calibrate any gyros 
+		 and start timers
+		 */
 		RobotMap.init();
 	   	RobotMap.gyro.calibrate();
-	   	
-		driveBase = new DriveBase();
-		/*insert poem here 
-		*/
+	   	/*
+	   	 The above 2 lines are required if the gyro is being used by any system,
+	   	 If it is not in place, the gyro is basically never turned on or accessed.
+	   	 */
+	   	 
+	   	driveBase = new DriveBase();
 		oi = new OI();
-		autonomousCommand = new AutonoumousCommandGroup();
-	    
+		autonomousCommand = new AutonoumousCommandGroup(); // declares what the autonomous period will run for its command!
+		// Declaring that the driveBase, or any subsystem including the oi
+		//is a new subsystem is required during the init process or they won't work
+		timer.reset();
+		timer.start();
+		
 	}
 	
 	public static void resetTimer() {
@@ -56,6 +75,7 @@ public class Robot extends TimedRobot {
 
 	public void autonomousPeriodic() {
 		Scheduler.getInstance().run();
+		//This runs the default command defined in robotInit()
 		
 	}
 
@@ -67,6 +87,8 @@ public class Robot extends TimedRobot {
 	}
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run();
+		//This runs any default commands defined in any subsystems, typically, only the driveBase/Train has one set
+		
 	}
 
 	public void testPeriodic() {
